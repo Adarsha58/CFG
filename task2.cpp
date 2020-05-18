1befff34605abaa83e15230fc2baf4c7ea335a55
Adarsha Poudel <adarsha@ucsb.edu>
2020-05-17 22:30:04 -0700

    hey

--- task2.cpp
+++ task2.cpp
@@ -4,8 +4,8 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <list>
 using namespace std;
-
 
 void insert(char lhs, string rhs, unordered_map<char, vector<string>> & cfg){
     unordered_map<char,vector<string>>::const_iterator got = cfg.find(lhs);
@@ -22,6 +22,22 @@ void insert(char lhs, string rhs, unordered_map<char, vector<string>> & cfg){
     }
 }
 
+void insert(char lhs, char rhs, unordered_map<char, vector<char>> & cfg){
+    unordered_map<char,vector<char>>::const_iterator got = cfg.find(lhs);
+    
+    if(got == cfg.end()){
+        vector<char> s;
+        s.push_back(rhs);
+        cfg.insert({lhs, s});
+    }else{
+        vector<char> v = got->second;
+        v.push_back(rhs);
+        cfg.erase(lhs);
+        cfg.insert({lhs, v});
+    }
+}
+
+
 
 int main()
 {
@@ -30,7 +46,7 @@ int main()
     int cfgSize = 0;
 
     unordered_map<char, vector<string>> cfg;
-    unordered_map<char, char> unitProduction;
+    unordered_map<char, vector<char>> unitProduction;
 
 
     for (int i = 0; i < 2 * nLines; i = i + 2)
@@ -46,7 +62,7 @@ int main()
         
         //checking if this is the unitProduciton
         if(rhs.size() == 1 && (rhs[0] - 'A' >= 0) && (rhs[0] - 'A' < 26)) {
-                unitProduction.insert({lhs, rhs[0]});
+                insert(lhs, rhs[0], unitProduction);
                 continue;
         }
         
@@ -57,27 +73,41 @@ int main()
 
     for (auto it = unitProduction.begin(); it != unitProduction.end(); ++it)
     {
-        unordered_set<char> s;
-        s.insert(it->first);
-        char reach = it->second;
+       char lhs = it->first;
+       list<char> tmp;
+       tmp.push_front(lhs);
+       unordered_set<char> visited;
+       visited.insert(lhs);
 
-        while(s.find(reach) == s.end()){
-            s.insert(reach);
-            unordered_map<char,vector<string>>::const_iterator got = cfg.find(reach);
-            if(got != cfg.end()){
-                vector<string> v = got->second;
-                for(auto it1 = v.begin(); it1 != v.end(); ++it1){
-                    insert(it->first, *it1, newProductions);
+        while(!tmp.empty()){
+            lhs = tmp.front();
+            tmp.remove(lhs);
+            unordered_map<char,vector<char>>::const_iterator got = unitProduction.find(lhs);
+            if(got != unitProduction.end()){
+                vector<char> v = got->second;
+                for(auto it1 = v.begin(); it1 != v.end(); it1++){
+                    if(visited.find(*it1) == visited.end()){
+                        visited.insert(*it1);
+                        tmp.push_front((*it1));
+                    }
                 }
-            }
-
-            if(unitProduction.find(reach) != unitProduction.end()){
-                unordered_map<char, char>::const_iterator r1 = unitProduction.find(reach);
-                reach = r1->second;
+            }else{
+                tmp.remove(lhs);
             }
         }
 
+        for(auto it2 = visited.begin(); it2 != visited.end(); it2++){
+            unordered_map<char,vector<string>>::const_iterator insertion = cfg.find(*it2);
+            if(insertion != cfg.end()){
+                vector<string> v1 = insertion->second;
+                for(auto it3 = v1.begin(); it3 != v1.end(); ++it3){
+                    insert(it->first, *it3, newProductions);
+                }
+            }   
+        }
+
     }
+
 
     cfg = newProductions;
 
@@ -88,16 +118,25 @@ int main()
         } 
     }
     cout<< cfgSize<<endl;
-    string cfgS = "";
+
+   
     for (auto it = cfg.begin(); it != cfg.end(); ++it)
     {
         for(auto it1 = (it->second).begin(); it1 != (it->second).end(); ++it1){
-            string s;
-            s = it->first;
-            cfgS += s + " " + (*it1) + "\n";
-        } 
+            cout<< it->first << " "<< *it1<< endl;
+        }
     }
-    cfgS = cfgS.substr(0, cfgS.size()-1);
-    cout<< cfgS;
+    
     return 0;
 }
+
+
+/*
+unordered_map<char,vector<string>>::const_iterator got = cfg.find(reach);
+            if(got != cfg.end()){
+                vector<string> v = got->second;
+                for(auto it1 = v.begin(); it1 != v.end(); ++it1){
+                    insert(it->first, *it1, newProductions);
+                }
+            }
+        */
